712f8e176e420cdee2016b7fabfca5c88be470a7
Parveen Sarana <parveenkaursarana@gmail.com>
2016-12-18 00:14:08 -0800

    change tests

--- tests/testthat/test_standard_deviation.R
+++ tests/testthat/test_standard_deviation.R
@@ -3,27 +3,27 @@
 context("Testing Standard Deviation")
 
 test_that("standard_deviation() calculates standard deviation correctly", {
-  
+
   #expected outputs
   a <- c(1, 1)
   b <- c(0, 0, 0)
   c <- c(0.5, 0.5)
   d <- c(1, 1, 3, 3)
   e <- c(-1, -1, -3, -3)
-  
+
   expect_equal(standard_error(a), 0)
   expect_equal(standard_error(b), 0)
   expect_equal(standard_error(c), 0)
   expect_equal(standard_error(d), 0.5)
   expect_equal(standard_error(e), 0.5)
-  
+
   #expected errors
-  expect_error(standard_deviation(c()), "Word")
+  expect_error(standard_deviation(c()), "Zero")
   expect_error(standard_deviation(c("a", "b", "c")), "type")
   expect_error(standard_deviation("a"), "type")
-  
+
   #if empty
-  expect_equal(standard_deviation(c()),NaN)
-              
+  expect_error(standard_deviation(c()),  "Error: Zero division")
+
 })
 
